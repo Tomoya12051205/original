89640251e37b5e2eb69c3986942643959ca50e1b
Tomoya12051205 <zorori1205@gmail.com>
2024-02-04 21:37:33 +0900

    話題投稿画面の作成

--- src/main/java/com/example/original/entity/AbstractEntity.java
+++ src/main/java/com/example/original/entity/AbstractEntity.java
@@ -11,21 +11,21 @@ import lombok.Data;
 @MappedSuperclass
 @Data
 public class AbstractEntity {
-	@Column(name= "created_at")
-	private Date createdAt;
-	
-	@Column(name = "updated_at")
-	private Date updatedAt;
-	
-	@PrePersist
-	public void onPrePersist() {
-		Date date = new Date();
-		setCreatedAt(date);
-		setUpdatedAt(date);
-	}
-	
-	@PreUpdate
-	public void onPreUpdate() {
-		setUpdatedAt(new Date());
-	}
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        Date date = new Date();
+        setCreatedAt(date);
+        setUpdatedAt(date);
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        setUpdatedAt(new Date());
+    }
 }
--- src/main/java/com/example/original/repository/UserRepository.java
+++ src/main/java/com/example/original/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.original.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	
-	User findByUsername(String username);
+
+    User findByUsername(String username);
 }
